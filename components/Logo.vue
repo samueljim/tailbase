e334feec9a6f909f91917571bd76af95a2665130
Sam Henry <thesamueljim@gmail.com>
2022-07-01 09:36:24 +1000

    logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="flex items-center">
-    <svg class="w-three h-three lg:w-five lg:h-five mr-one" viewBox="0 0 140 140" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect y="136" width="140" height="4" fill="#E4E4E4"/>
-      <rect width="140" height="4" fill="#E4E4E4"/>
-      <rect y="68" width="140" height="4" fill="#E4E4E4"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M0 0V30H40V95C40 119.853 60.1472 140 85 140C109.853 140 130 119.853 130 95C130 70.1472 109.853 50 85 50C79.7405 50 74.6917 50.9023 70 52.5606V0H0ZM85 110C76.7157 110 70 103.284 70 95C70 86.7157 76.7157 80 85 80C93.2843 80 100 86.7157 100 95C100 103.284 93.2843 110 85 110Z" fill="#0ED7B5"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M70 52.5606V95C70 86.7157 76.7157 80 85 80C93.2843 80 100 86.7157 100 95V52.5606C95.3083 50.9023 90.2595 50 85 50C79.7405 50 74.6917 50.9023 70 52.5606Z" fill="url(#paint0_linear)"/>
-      <defs>
-        <linearGradient id="paint0_linear" x1="70" y1="65.5" x2="100" y2="65.5" gradientUnits="userSpaceOnUse">
-          <stop stop-opacity="0.2"/>
-          <stop offset="1" stop-color="#C4C4C4" stop-opacity="0"/>
-        </linearGradient>
-      </defs>
-    </svg>
-    <span class="text-4xl lg:text-5xl leading-none font-medium">Tailbase</span>
+    <img class="w-three h-three lg:w-five lg:h-five mr-one"  src='https://ca.slack-edge.com/T8NR5E28Z-U038VDGT75Y-5b11d13c2dd8-512'  />
+    <span class="text-4xl lg:text-5xl leading-none font-medium">Typescale</span>
   </div>
 </template>
