890b0114cc13de2b644323a6ec72e3c02ecdd17c
Sam Henry <thesamueljim@gmail.com>
2022-07-01 09:31:35 +1000

    added credit

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
 
     <h1 class="mx-auto text-4xl lg:text-5xl max-w-4xl leading-tight text-center mt-six mb-three font-bold">
-      Tailwind config generator for a typographic scale with vertical rhythm.
+      Tailwind config generator for a typographic scale with vertical rhythm. 110% made by Sam middlename Henry
     </h1>
     <p class="mx-auto max-w-3xl text-lg text-gray-600 text-center mb-three">This tool will help you create a solid foundation for your <a class="text-gray-700 underline" href="https://tailwindcss.com" aria-label="Check out Tailwind">Tailwind</a> config, where line-height, font-size, padding & margin always will match. Thats all you need to create beautiful vertical rhythm in Tailwind, as seen on my personal <a class="text-gray-700 underline" href="https://samuelhorn.com" aria-label="Check out my site">site</a>.</p>
     
